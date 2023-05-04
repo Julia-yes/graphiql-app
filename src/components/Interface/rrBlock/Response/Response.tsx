02128a472a449055685be4b4890491e3c249fe00
Julia-yes <ec.ho@mail.ru>
2023-05-04 13:37:45 +0400

    feat: add total design

---
+++ src/components/Interface/rrBlock/Response/Response.tsx
@@ -0,0 +1,9 @@
+import stylesCommon from '../RRBlock.module.scss';
+
+export const Response = () => {
+  return (
+    <section className={stylesCommon.wrapper}>
+      <h3 className={stylesCommon.title}>Response</h3>
+    </section>
+  );
+};
