63876ae9cd124782fead33efc4148fd7439a426e
Mikhail Sprygin <wave103x@gmail.com>
2023-05-07 16:04:36 +0300

    feat: add welcom page expample login behavior

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,19 +1,32 @@
 import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
-import { logout } from '../firebase/firebase';
+import { auth, logout } from '../firebase/firebase';
 
 import styles from './Welcome.module.scss';
 
 export const Welcome = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <div className={styles.welc}>
       <div>Welcome</div>
-      <NavLink to='/login'>Sign In</NavLink>
-      <div></div>
-      <NavLink to='/graphiQL'>Graphi</NavLink>
-      <button style={{ display: 'block' }} onClick={logout}>
-        logout
-      </button>
+      {user ? (
+        <>
+          <NavLink to='/graphiQL'>Graphi</NavLink>
+          <button style={{ display: 'block' }} onClick={logout}>
+            logout
+          </button>
+        </>
+      ) : (
+        <>
+          <NavLink to='/login'>Sign In</NavLink>
+          <br />
+          <NavLink to='/register'>Sign Up</NavLink>
+        </>
+      )}
     </div>
   );
 };
